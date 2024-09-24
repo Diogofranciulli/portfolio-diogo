d44d1806c4cffb98ca139098ee402e267c0d8a30
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-24 20:34:59 -0300

    Ajuste footer

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -8,7 +8,7 @@ function Nav() {
   const isActive = (path) => location.pathname === path;
 
   return (
-    <nav className="flex w-full text-4xl font-bold justify-end p-4 bg-white shadow-lg space-x-8">
+    <nav className="flex flex-col md:flex-row w-full text-2xl md:text-4xl font-bold justify-end p-4 bg-white shadow-lg space-y-4 md:space-y-0 md:space-x-8">
       <Link
         to="/"
         className={`${
@@ -20,7 +20,7 @@ function Nav() {
       <Link
         to="/projetos"
         className={`${
-          isActive('/projetos') ? ' text-blue-500 border-b-2 border-blue-500' : 'text-black'
+          isActive('/projetos') ? 'text-blue-500 border-b-2 border-blue-500' : 'text-black'
         }`}
       >
         Projetos
@@ -37,10 +37,11 @@ function Nav() {
   );
 }
 
+
 function Footer() {
   return (
     <footer className="py-4 mt-auto w-full bg-white text-center flex justify-between items-center px-8">
-      <p>copyright © 2024</p>
+      <p>Copyright © 2024</p>
       <div className="flex space-x-4">
         <a href="https://github.com/Diogofranciulli" target="_blank" rel="noopener noreferrer">
           <FaGithub className="w-6 h-6" />
